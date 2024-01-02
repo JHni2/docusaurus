1ae44ee0cf476ed344008b5682d984a30f41c831
JHni2 <wlgus0260@naver.com>
2024-01-02 16:38:15 +0900

    [feat] ~고객지원

---
+++ src/constant/ClientConnectProgram.js
@@ -0,0 +1,10 @@
+const ClientConnectProgram = [
+  { 기능: 'SSH 클라이언트', 설명: 'SSH 연결을 시도할 때 사용하는 프로그램을 선택할 수 있다.' },
+  { 기능: 'BROWSER', 설명: '브라우저를 열 때 사용하는 프로그램을 선택할 수 있다.' },
+  { 기능: 'SSH PATH', 설명: '"Putty", "Xshell", "SecureCRT", "MobaxTerm" 경로를 선택할 수 있다.' },
+  { 기능: 'SFTP PATH', 설명: '"FileZilla" 경로를 선택할 수 있다.' },
+  { 기능: 'RDP PATH', 설명: '"MSTSC" 경로를 선택할 수 있다' },
+  { 기능: 'BROWSER PATH', 설명: '"Chrome", "Edge" 경로를 선택할 수 있다.' },
+];
+
+export default ClientConnectProgram;
